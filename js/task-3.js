b45ccc2580f8b9ae8e7e015ff754efed498e2b88
t-matveentsev <tmatveentsev@gmail.com>
2024-02-25 00:16:25 +0100

    hw-done

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,25 @@
 'use strict';
 
-function filterArray(numbers, value) {
-    let array = [];
-    for (let i = 0; i < numbers.length; i++) {
-        if (numbers[i] > value) {
-            array.push(numbers[i]);
-        }
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+    changeUsername(newName) {
+        if (profile.username != newName) {
+          profile.username = newName
+      }
+    },
+    updatePlayTime(hours) {
+        profile.playTime += hours
+    },
+    getInfo() {
+        return `${profile.username} has ${profile.playTime} active hours!`
     }
-    return array;
-}
+};
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
